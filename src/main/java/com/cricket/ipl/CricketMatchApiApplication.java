ff2d7f4e5915fa284cc24d7895340deb007e0e88
Santu Jana <115920400+akku27-cse@users.noreply.github.com>
2024-08-09 08:49:57 +0000

    spring

---
+++ src/main/java/com/cricket/ipl/CricketMatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.ipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketMatchApiApplication.class, args);
+	}
+
+}
